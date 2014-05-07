9d29c0430fff4a261cf622a2ec145025bf8bd84d
John Howe <johnhowe@gmail.com>
2014-05-07 13:58:26 +1200

    blueterm_1.1.0.tar.gz

--- src/es/pymasde/blueterm/DeviceListActivity.java
+++ src/es/pymasde/blueterm/DeviceListActivity.java
@@ -155,20 +155,34 @@ public class DeviceListActivity extends Activity {
     // The on-click listener for all devices in the ListViews
     private OnItemClickListener mDeviceClickListener = new OnItemClickListener() {
         public void onItemClick(AdapterView<?> av, View v, int arg2, long arg3) {
+        	String noDevicesPaired = getResources().getText(R.string.none_paired).toString();
+            String noDevicesFound = getResources().getText(R.string.none_found).toString();
+
+        	
             // Cancel discovery because it's costly and we're about to connect
             mBtAdapter.cancelDiscovery();
-
-            // Get the device MAC address, which is the last 17 chars in the View
+        	
             String info = ((TextView) v).getText().toString();
-            String address = info.substring(info.length() - 17);
+            
+            if ( (info != noDevicesPaired)  &&  (info != noDevicesFound) ){
+            
+                if (info.length() >= 17) {
+                    // Get the device MAC address, which is the last 17 chars in the View
+                    String address = info.substring(info.length() - 17);
 
-            // Create the result Intent and include the MAC address
-            Intent intent = new Intent();
-            intent.putExtra(EXTRA_DEVICE_ADDRESS, address);
+                    // Create the result Intent and include the MAC address
+                    Intent intent = new Intent();
+                    intent.putExtra(EXTRA_DEVICE_ADDRESS, address);
 
-            // Set result and finish this Activity
-            setResult(Activity.RESULT_OK, intent);
-            finish();
+                    // Set result and finish this Activity
+                    setResult(Activity.RESULT_OK, intent);
+                }
+                else {
+                    setResult(Activity.RESULT_CANCELED);
+                }
+            	
+                finish();
+            }
         }
     };
 
